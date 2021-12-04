b9d53dbbf1690f1bab7eb332fa63d2a401d5609d
DCAZAYUS <daniel.cazayus@cea.fr>
2021-12-04 04:06:10 +0100

    Day three clarification et simplification du code

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -7,37 +7,33 @@ fn main() {
     part_two(&lines);
 }
 
-fn find_most_and_least_common_bit_as_vec(lines: &Vec<&str>) -> (Vec<char>, Vec<char>) {
-    let mut ones_vec = Vec::new();
-    let mut zeroes_vec = Vec::new();
-    for line in lines {
-        for (index, b) in line.bytes().enumerate() {
-            if ones_vec.get(index).is_none() {
-                ones_vec.push(0);
-                zeroes_vec.push(0);
-            }
-            if b == b'1' {
-                ones_vec[index] += 1;
-            } else {
-                zeroes_vec[index] += 1
-            }
-        }
-    }
-    let mut output: (Vec<char>, Vec<char>) = (vec![], vec![]);
-    for i in 0..ones_vec.len() {
-        if ones_vec[i] >= zeroes_vec[i] {
-            output.0.push('1');
-            output.1.push('0');
+fn find_most_common_bit_as_vec(lines: &Vec<&str>) -> Vec<char> {
+    let line_length = lines[0].len();
+    let mut output: Vec<char> = Vec::new();
+    for index in 0..line_length {
+        let sum: i32 = lines
+            .iter()
+            .map(|b| b[index..index + 1].parse::<i32>().unwrap())
+            .sum();
+        if sum as f64 >= lines.len() as f64 / 2.0 {
+            output.push('1');
         } else {
-            output.0.push('0');
-            output.1.push('1');
+            output.push('0');
         }
     }
     return output;
 }
 
+fn convert_most_common_to_least_common(most_common: &Vec<char>) -> Vec<char> {
+    return most_common
+        .iter()
+        .map(|&c| if c == '1' { '0' } else { '1' })
+        .collect();
+}
+
 fn part_one(lines: &Vec<&str>) {
-    let (most_common_bits, least_common_bits) = find_most_and_least_common_bit_as_vec(&lines);
+    let most_common_bits = find_most_common_bit_as_vec(&lines);
+    let least_common_bits = convert_most_common_to_least_common(&most_common_bits);
     let gamma =
         isize::from_str_radix(most_common_bits.iter().collect::<String>().as_str(), 2).unwrap();
     let epsilon =
@@ -49,7 +45,7 @@ fn part_two(lines: &Vec<&str>) {
     let mut current_bit_under_scrutiny = 0;
     let mut mutable_lines: Vec<&str> = lines.clone();
     while mutable_lines.len() > 1 {
-        let (most_common_bits, _) = find_most_and_least_common_bit_as_vec(&mutable_lines);
+        let most_common_bits = find_most_common_bit_as_vec(&mutable_lines);
         mutable_lines.retain(|line| {
             line.chars().nth(current_bit_under_scrutiny).unwrap()
                 == most_common_bits[current_bit_under_scrutiny]
@@ -60,7 +56,8 @@ fn part_two(lines: &Vec<&str>) {
     let mut current_bit_under_scrutiny = 0;
     let mut mutable_lines: Vec<&str> = lines.clone();
     while mutable_lines.len() > 1 {
-        let (_, least_common_bit) = find_most_and_least_common_bit_as_vec(&mutable_lines);
+        let least_common_bit =
+            convert_most_common_to_least_common(&find_most_common_bit_as_vec(&mutable_lines));
         mutable_lines.retain(|line| {
             line.chars().nth(current_bit_under_scrutiny).unwrap()
                 == least_common_bit[current_bit_under_scrutiny]
