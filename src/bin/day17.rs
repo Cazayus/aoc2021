85362d6db24d0688f0242b7282e6f937490d7ae1
DCAZAYUS <daniel.cazayus@cea.fr>
2021-12-20 19:51:20 +0100

    Day 17 optimisation

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -1,92 +1,50 @@
+use regex::Regex;
+
 const DATA: &str = include_str!("../inputs/day17.txt");
-const MAX_VY_TESTED: u32 = 500;
 
 fn main() {
     println!("part 1: {}", part_one(DATA));
     println!("part 2: {}", part_two(DATA));
 }
 
-fn part_one(data: &str) -> i32 {
-    let data = parse_data(data);
-    let (_, _) = find_minmax_vx(data[0], data[1]);
-    let (_, max_vy) = find_minmax_vy(data[2], data[3]);
-    max_vy * (max_vy + 1) / 2
+fn part_one(data: &str) -> isize {
+    let [_, _, min_y_target, _] = parse_data(data);
+    // the probe comes back at Y=0 with the same speed you shoot it up but in reverse
+    // in order not to overshoot, the maximum allowed speed is then abs(min_y_target) - 1 and the
+    // next step would see us barely touching the bottom side of the area
+    let max_vy = -min_y_target - 1;
+    max_vy * (max_vy + 1) / 2 // Simple somme arithmétique de raison 1
 }
 
-fn part_two(data: &str) -> usize {
-    let data = parse_data(data);
-    let (min_vx, max_vx) = find_minmax_vx(data[0], data[1]);
-    let (min_vy, max_vy) = find_minmax_vy(data[2], data[3]);
+fn part_two(data: &str) -> isize {
+    let [min_x_area, max_x_area, min_y_area, max_y_area] = parse_data(data);
     let mut count = 0;
-    for dx in min_vx..=max_vx {
-        for dy in min_vy..=max_vy {
-            let mut pos = (0, 0);
-            let mut local_diff = (dx, dy);
-            while pos.1 > data[2] {
-                pos.0 += local_diff.0;
-                pos.1 += local_diff.1;
-                if (data[0]..=data[1]).contains(&pos.0) && (data[2]..=data[3]).contains(&pos.1) {
+    // No need to check beyond a x velocity of max_x_target (we should shoot beyond the area)
+    for init_vx in 0..=max_x_area {
+        // No need to check before a y velocity of min_y_target (we would shoot below the area)
+        for init_vy in min_y_area..=(-min_y_area - 1) {
+            let (mut x, mut y) = (0, 0);
+            let (mut vx, mut vy) = (init_vx, init_vy);
+            while x <= max_x_area && y > min_y_area {
+                x += vx;
+                y += vy;
+                vx -= vx.signum();
+                vy -= 1;
+                if (min_x_area..=max_x_area).contains(&x) && (min_y_area..=max_y_area).contains(&y)
+                {
                     count += 1;
                     break;
                 }
-                if local_diff.0 > 0 {
-                    local_diff.0 -= 1;
-                }
-                local_diff.1 -= 1;
             }
         }
     }
     count
 }
 
-fn parse_data(data: &str) -> Vec<i32> {
-    data[15..]
-        .to_string()
-        .split(", y=")
-        .flat_map(|split| {
-            split
-                .split("..")
-                .map(|split| split.parse().unwrap())
-                .collect::<Vec<_>>()
-        })
-        .collect()
-}
-
-fn find_minmax_vx(min_x_target: i32, max_x_target: i32) -> (i32, i32) {
-    let mut min_vx = 0;
-    for dx in 1..max_x_target {
-        let mut pos_x = 0;
-        let mut local_dx = dx;
-        while local_dx > 0 && pos_x < max_x_target {
-            pos_x += local_dx;
-            local_dx -= 1;
-            if (min_x_target..=max_x_target).contains(&pos_x) {
-                if min_vx == 0 {
-                    min_vx = dx;
-                    break;
-                }
-            }
-        }
-    }
-    (min_vx, max_x_target)
-}
-
-fn find_minmax_vy(min_y_target: i32, max_y_target: i32) -> (i32, i32) {
-    let mut max_vy = 0;
-    let mut latest_working_dy = 0;
-    for dy in 1..MAX_VY_TESTED {
-        let mut pos_y = 0;
-        let mut local_dy = dy as i32;
-        while pos_y > min_y_target {
-            pos_y += local_dy;
-            local_dy -= 1;
-            if (min_y_target..=max_y_target).contains(&pos_y) {
-                latest_working_dy = dy as i32;
-            }
-        }
-        max_vy = latest_working_dy;
-    }
-    (min_y_target, max_vy)
+fn parse_data(data: &str) -> [isize; 4] {
+    let pat = Regex::new(r"target area: x=(-?\d+)\.\.(-?\d+), y=(-?\d+)\.\.(-?\d+)").unwrap();
+    let caps = pat.captures(data).unwrap();
+    [&caps[1], &caps[2], &caps[3], &caps[4]].map(|s| s.parse().unwrap())
 }
 
 #[cfg(test)]
