7203f013cf4414d5aeeb64a228dabb43a0c3b3bd
DCAZAYUS <daniel.cazayus@cea.fr>
2021-12-28 15:30:32 +0100

    Scaffolding pour 23, 24, 25

---
+++ src/bin/day24.rs
@@ -0,0 +1,30 @@
+const DATA: &str = include_str!("../inputs/day24.txt");
+
+fn main() {
+    let (one, two) = solve(DATA);
+    println!("part 1: {}", one);
+    println!("part 2: {}", two);
+}
+
+fn solve(data: &str) -> (usize, usize) {
+    let one = data.len();
+    let two = data.len();
+    (one, two)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const SAMPLE_DATA: &str = include_str!("../inputs/day24_sample.txt");
+
+    #[test]
+    fn sample() {
+        assert_eq!(solve(SAMPLE_DATA), (0, 0));
+    }
+
+    #[test]
+    fn case() {
+        assert_eq!(solve(DATA), (0, 0));
+    }
+}
