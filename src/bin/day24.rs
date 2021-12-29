582120d0441aa9774fc7808217b4f2b4919ace7f
DCAZAYUS <daniel.cazayus@cea.fr>
2021-12-29 12:06:21 +0100

    Day 24 start (flemme)

--- src/bin/day24.rs
+++ src/bin/day24.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 const DATA: &str = include_str!("../inputs/day24.txt");
 
 fn main() {
@@ -7,6 +9,35 @@ fn main() {
 }
 
 fn solve(data: &str) -> (usize, usize) {
+    let input = data
+        .split("inp w\r\n")
+        .filter(|bloc| bloc.len() > 0)
+        .inspect(|bloc| println!("{} & {}", bloc.trim(), bloc.len()))
+        .map(|bloc| bloc.trim().lines().collect())
+        .map(|instructions: Vec<&str>| {
+            let mut toto = HashMap::new();
+            for x in 1..9 {
+                toto.clear();
+                toto.insert("w", x);
+                toto.insert("x", 0);
+                toto.insert("y", 0);
+                toto.insert("z", 0);
+                for instruction in instructions.clone() {
+                    let split = instruction.split_whitespace().collect::<Vec<_>>();
+                    let a = toto.entry(split[1]);
+                    match split[0] {
+                        "add" => {}
+                        "mul" => {}
+                        "div" => {}
+                        "mod" => {}
+                        "eql" => {}
+                        _ => {}
+                    }
+                }
+            }
+        })
+        .collect::<Vec<_>>();
+    dbg!(input.len());
     let one = data.len();
     let two = data.len();
     (one, two)
